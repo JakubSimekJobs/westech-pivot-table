587e717949dc9e80f872feec54088b0fcad4beb8
Jakub Simek <jakub.simek@odyzeo.com>
2024-08-27 16:31:11 +0200

    Add .env configuration

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const title = ref(import.meta.env.VITE_PROJECT_TITLE);
+const author = ref(import.meta.env.VITE_AUTHOR);
 </script>
 
 <template>
   <header>
-    Init
+    <h1 v-text="title"></h1>
   </header>
   <main>
   </main>
+  <footer>Made by: {{author}}</footer>
 </template>
 
